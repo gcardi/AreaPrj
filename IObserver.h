bce9569c1b4213039b6eddb2980f9fc9261fe685
Giuliano <giulicard@gmail.com>
2021-12-27 23:16:20 +0100

    Modified IView and IObserver to get rid of stupid error of EMB clang based compilers

--- IObserver.h
+++ IObserver.h
@@ -1,10 +1,23 @@
 #ifndef IObserverH
 #define IObserverH
 
+#include <System.hpp>  // <-- for IUnknown
+
 namespace AreaPrj {
 
-class IObserver {
+class IObserver : public IUnknown {
 public:
+    // These three stupid methods are needed to get rid of even more stupid
+    // warnings from the Embarcadero clang compilers that claim that the
+    // classes that a form derives from and that have an 'I' at the beginning
+    // of their name themselves have to derive from IUnknown.
+    //
+    // [bcc(32c|64) Warning]: Interface 'IObserver' does not derive
+    // from IUnknown. (Interfaces should derive from IUnknown)
+    virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void ** ppvObject) override { return {}; }
+    virtual ULONG   STDMETHODCALLTYPE AddRef() override { return {}; }
+    virtual ULONG   STDMETHODCALLTYPE Release() override { return {}; }
+
     void Notify() { DoNotify(); }
 protected:
     virtual void DoNotify() = 0;
--- IView.h
+++ IView.h
@@ -3,13 +3,23 @@
 
 #include <System.hpp>
 
-
  namespace AreaPrj {
 
 class IModel;
 
-class IView {
+class IView : public IUnknown {
 public:
+    // These three stupid methods are needed to get rid of even more stupid
+    // warnings from the Embarcadero clang compilers that claim that the
+    // classes that a form derives from and that have an 'I' at the beginning
+    // of their name themselves have to derive from IUnknown.
+    //
+    // [bcc(32c|64) Warning]: Interface 'IView' does not derive
+    // from IUnknown. (Interfaces should derive from IUnknown)
+    virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void ** ppvObject) override { return {}; }
+    virtual ULONG   STDMETHODCALLTYPE AddRef() override { return {}; }
+    virtual ULONG   STDMETHODCALLTYPE Release() override { return {}; }
+
     /** \brief Legge il testo dell'interfaccia utente
       * \return La stringa contenente il testo
       *
